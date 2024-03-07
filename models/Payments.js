29b3b9c13567d893bbd9c909afcea5bb351e9e44
Abraham <abrahamnhier97@gmail.com>
2024-03-07 10:38:22 +0300

    defined payment schema

---
+++ models/Payments.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const PaymentSchema  =  new mongoose.Schema({
+    amount: {
+        type: Number,
+        required: true,
+    },
+    driverId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Driver',
+        required: true,
+    },
+    clientId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Client',
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: ['pending', 'accepted', 'declined'],
+        default: 'pending',
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model('Payment', PaymentSchema);

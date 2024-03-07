090f6425d44eda5014528e5be4b24bd88ab4345a
Abraham <abrahamnhier97@gmail.com>
2024-03-07 10:58:29 +0300

    error handlers

---
+++ errors/index.js
@@ -0,0 +1,9 @@
+const BadRequest = require('./BadRequest');
+const CustomeError = require('./CustomError');
+const NotFoundError = require('./not-found')
+
+module.exports = {
+    BadRequest,
+    CustomeError,
+    NotFoundError,
+}

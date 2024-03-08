2be8790ccbb7183e522def9b6566157193005957
Abraham <abrahamnhier97@gmail.com>
2024-03-08 07:34:41 +0300

    user routes controllers

--- controllers/UserControllers.js
+++ controllers/UserControllers.js
@@ -20,3 +20,38 @@ const registerUser = async (req, res) => {
     attachCookiesToResponse({ res, user: tokenUser})
     res.status(StatusCodes.CREATED).json({user: tokenUser});
 };
+//login user
+
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    //check email password
+    if (!email || !password) {
+        throw new CustomError.BadRequest('Email and password are required');
+    }
+    const user = await User.findOne({ email })
+    if (!user) {
+        throw new CustomError.BadRequest('Email or password is incorrect');
+    }
+    //check password
+    const isPasswordCorrect = await user.comparePassword(password);
+    if (!isPasswordCorrect) {
+        throw new CustomError.BadRequest('password is incorrect');
+    }
+    const tokenUser = createTokenUser(user);
+    attachCookiesToResponse({ res, user: tokenUser})
+    res.status(StatusCodes.OK).json({user: tokenUser});
+}
+//logout user
+const logout = async (req, res) => {
+    res.cookie('token', 'logout', {
+        expires: new Date(Date.now() + 1000),
+        httpOnly: true
+    });
+    res.status(StatusCodes.OK).json({ msg: 'user logged out' });
+}
+
+module.exports = {
+    registerUser,
+    login,
+    logout
+}
